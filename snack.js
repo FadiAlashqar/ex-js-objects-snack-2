4076a98f5241d734689a40f01d10c11162e581f4
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-07-18 17:40:45 +0200

    [UPD] snack 1

---
+++ snack.js
@@ -0,0 +1,3 @@
+// SNACK 1
+// In riga 7 il console.log ci mostrerà 'Double Cheese Burger', nel secondo invece 500.
+// In memoria è stato creato un solo oggetto, il secondo è solamente un riferimento, non una copia indipendete.
