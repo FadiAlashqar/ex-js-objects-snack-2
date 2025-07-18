c9f398a4b9799991041a3aeeee9cd047deea68d1
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-07-18 17:53:08 +0200

    [UPD] snack 2

--- snack.js
+++ snack.js
@@ -1,3 +1,7 @@
 // SNACK 1
 // In riga 7 il console.log ci mostrerà 'Double Cheese Burger', nel secondo invece 500.
 // In memoria è stato creato un solo oggetto, il secondo è solamente un riferimento, non una copia indipendete.
+
+// SNACK 2
+// In entrambi i console.log quello che vedremo sarà "Salad", questo perchè con lo spread operatore viene effettuata una copia solo del primo livello delle propreità dell'oggetto, array/ogetti annidati rimagono dei riferimenti, se andiamo a fare una modifica sul secondo, questa avverrà anche sul primo.
+// In memoria sono stati creati due oggetti in questo caso.
