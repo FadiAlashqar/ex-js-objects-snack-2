2e8995c129aadc006b8b243eb0932d0a70db23bd
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-07-18 17:55:26 +0200

    [UPD] snaack 3

--- snack.js
+++ snack.js
@@ -5,3 +5,6 @@
 // SNACK 2
 // In entrambi i console.log quello che vedremo sarà "Salad", questo perchè con lo spread operatore viene effettuata una copia solo del primo livello delle propreità dell'oggetto, array/ogetti annidati rimagono dei riferimenti, se andiamo a fare una modifica sul secondo, questa avverrà anche sul primo.
 // In memoria sono stati creati due oggetti in questo caso.
+
+// SNACK 3
+// In questo caso sono stati creati tre oggetti in memoria, il metodo utilizzato effettua una copia indipendete dell'oggetto.
